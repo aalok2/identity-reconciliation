897deb8c1816121fa1a2f1fb71c45826feff13e3
aalok-hybr1d <aalok@hybr1d.io>
2024-11-16 21:57:41 +0530

    Fixed the deployment issues;

--- src/models/db.ts
+++ src/models/db.ts
@@ -4,11 +4,7 @@ import { Sequelize, DataTypes } from "sequelize";
 import { Config } from "../config";
 import { fileURLToPath, pathToFileURL } from "url";
 
-
 // Define __filename and __dirname in a CommonJS-compatible way
-const __filename = path.resolve(process.argv[1]);
-const __dirname = path.dirname(__filename);
-
 
 const basename = path.basename(__filename);
 const db: { [key: string]: any } = {};
@@ -36,33 +32,29 @@ async function initializeDatabase() {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-
-    const modelFiles = fs.readdirSync(__dirname).filter((file) => {
-      return (
-        file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".ts"
-      );
-    });
-
-    for (const file of modelFiles) {
-      const modelPath = pathToFileURL(path.join(__dirname, file)).href;
-      const modelModule = await import(modelPath);
-      const model = modelModule.default(sequelize, DataTypes);
-      db[model.name] = model;
-    }
-
-    Object.keys(db).forEach((modelName) => {
-      if (db[modelName].associate) {
-        db[modelName].associate(db);
-      }
-    });
-
-    db.sequelize = sequelize;
-    db.Sequelize = Sequelize;
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 }
 
-initializeDatabase();
+fs.readdirSync(__dirname)
+  .filter((file: string) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".ts"
+    );
+  })
+  .forEach((file: string) => {
+    const model = require(path.join(__dirname, file))(sequelize, DataTypes);
+    db[model.name] = model;
+  });
 
+Object.keys(db).forEach((modelName: string) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 export default db;
+initializeDatabase();
