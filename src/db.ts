8e0a460e3adaaafeead2cd8f3f935a7e50e4eb08
aalok-hybr1d <aalok@hybr1d.io>
2024-11-17 13:10:56 +0530

    Added SSL connection enforcement

--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
 const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
 
 const dbClient = new Pool({
@@ -13,6 +12,9 @@ const dbClient = new Pool({
   host: DB_HOST,
   port: Number(DB_PORT),
   database: DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: true, // Use `true` in production for stricter security
+  },
 });
 
 export default dbClient;
