ad8aa0c4db783699327e214fe2c45390616f4bcb
aalok <aalok150124@gmail.com>
2025-01-09 11:43:06 +0530

    Added something

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const Config = {
     DATABASE: process.env.DB_DATABASE || "contact",
     USERNAME: process.env.DB_USERNAME || "postgres",
     PASSWORD: process.env.DB_PASSWORD || "12345",
-    HOST: process.env.DB_HOST || "localhost",
+    HOST: process.env.DB_HOST || "localhost:8080",
     PORT: Number(process.env.DB_PORT) || 5432,
   },
 };
